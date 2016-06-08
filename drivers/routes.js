928589037d29c6b5be7acd517008f6f61bd39ae1
k6k <florian7cherel@gmail.com>
2016-06-08 17:36:00 +0200

    Real time drawing

--- drivers/routes.js
+++ drivers/routes.js
@@ -1,7 +1,9 @@
+var exp = require('express');
 module.exports = function(app){
 	return {
 		create : function(express){
 			var _routes = app.config.routes;
+			express.use('/assets',exp.static('assets'));
 			for (var i = _routes.length - 1; i >= 0; i--) {	
 				(function(_r){
 					express.get(_r.path,function(req,res){
--- services/messenger.js
+++ services/messenger.js
@@ -1,7 +1,10 @@
 module.exports = function(app){
 	return{
-		send : function(msg){
-			app.socket.io.emit('receive',msg);
+		draw_line : function(msg){
+			app.socket.io.emit('draw_line',msg);
+		},
+		leave : function(point){
+			app.socket.io.emit('leave',point);
 		}
 	}
 }
